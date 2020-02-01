be23c956aa36662d404fcd33f7fd5a37e26a89d1
Vincent Sanders <vincesanders@hotmail.com>
2020-02-01 11:43:40 -0600

    Added some comments with TODO messages.

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export default () => {
     return axios.create({
+        //TODO: update to correct endpoint
         baseURL: "http://need/endpoint",
         headers: {
             authorization: localStorage.getItem("token")
