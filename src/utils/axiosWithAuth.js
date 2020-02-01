ad3d74ec1f7532f2907bcb642d7c911efa4af93c
Vincent Sanders <vincesanders@hotmail.com>
2020-02-01 11:33:37 -0600

    Added utils folder with index.js. Added axiosWithAuth to utils.

---
+++ src/utils/axiosWithAuth.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default () => {
+    return axios.create({
+        baseURL: "http://need/endpoint",
+        headers: {
+            authorization: localStorage.getItem("token")
+        }
+    });
+}
