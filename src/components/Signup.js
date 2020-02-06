e648849b98816ecf7fa593aefdbb29785c09f283
Richard Wang <richardwang2050@gmail.com>
2020-02-05 17:04:22 -0800

    Polished listing

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -24,7 +24,7 @@ const Signup = (props) => {
         event.preventDefault();
 
         axios
-        .post(`https://fast-scrubland-91418.herokuapp.com/api/auth/registerHost`, {username: values.firstName, password: values.password})
+        .post(`https://fast-scrubland-91418.herokuapp.com/api/auth/registerHost`, {username: values.username, password: values.password})
         .then(response => {
             history.push('/login');
         })
